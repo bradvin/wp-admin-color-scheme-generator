f4e58ff0abf61ba9c06b0d449870c1723c4e1378
Brad Vincent <bradvin@gmail.com>
2013-12-24 00:51:53 +0200

    form validation js

--- js/admin-color-scheme-generator.js
+++ js/admin-color-scheme-generator.js
@@ -1,3 +1,12 @@
 jQuery(function($) {
 	$('.acsg-colorpicker').minicolors();
+
+	$('#acsg_form input[type=submit]').click(function(e) {
+		if ($('#scheme_name').val() == '') { alert('Please enter a color scheme name'); return false; }
+		else if ($('#scheme_author').val() == '') { alert('Please enter a color scheme author'); return false; }
+		else if ($('#scheme_base_color').val() == '') { alert('Please choose a base color'); return false; }
+		else if ($('#scheme_highlight_color').val() == '') { alert('Please choose a highlight color'); return false; }
+		else if ($('#scheme_notification_color').val() == '') { alert('Please choose a notification color'); return false; }
+		else if ($('#scheme_action_color').val() == '') { alert('Please choose an action color'); return false; }
+	});
 });
